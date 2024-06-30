5e0e9964db65ed2ccb391aed90fe736de61ccf80
DRFS125 <deafrelationfriends125@gmail.com>
2024-06-30 16:21:56 +0530

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,14 @@
+var elem = document.getElementById('html');
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+document.addEventListener("13", e =>{
+  openFullscreen();
+});
